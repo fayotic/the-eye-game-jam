23bdddd9b96571d4b93f7dcbdff65753a2d1f6cf
ImadCrncevic <crncevic.imad@gmail.com>
2026-02-23 21:16:15 +0100

    Added doors to lvl and added monster to lvl

--- components/gateswitch/gate/gate.gd
+++ components/gateswitch/gate/gate.gd
@@ -1,9 +1,15 @@
+@tool
 class_name Gate
 extends Node3D
 
+@export var material:Material:
+	set(value):
+		material = value
+		_update_material()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_update_material()
 	pass # Replace with function body.
 
 
@@ -15,3 +21,10 @@ func open() -> void:
 	print ("open gate")
 	queue_free()
 	pass
+
+func _update_material():
+	if not is_inside_tree():
+		return
+	
+	if has_node("CSGBox3D"):
+		$CSGBox3D.material = material
--- components/gateswitch/gateswitch.gd
+++ components/gateswitch/gateswitch.gd
@@ -1,16 +1,20 @@
+@tool
 extends Node3D
 
 @export var gates: Array[Gate] = []
 var isClicked = false
+@export var material: Material:
+	set(value):
+		material = value
+		_update_material()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if gates.is_empty():
 		push_warning("No gates set. Set some in the inspector of ", self)
-		return
 		
+	_update_material()	
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -23,3 +27,10 @@ func interact() -> void:
 			gate.open()
 		
 		isClicked = true
+		
+func _update_material():
+	if not is_inside_tree():
+		return
+	
+	if has_node("CSGBox3D"):
+		$CSGBox3D.material = material
